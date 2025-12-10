a2e84a4dfc6cf22a8754e579878bb5b3ff4b16fd
NoahS-132 <769161@student.dusd.net>
2025-12-10 12:17:12 -0800

    Finshed

--- SanchezFlappyFinalP4/Assets/Scripts/Bird.cs
+++ SanchezFlappyFinalP4/Assets/Scripts/Bird.cs
@@ -2,15 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Bird : MonoBehaviour
-{
-    private bool isDead; = false;
-    private Rigidbody2D rb2d;
+public class Bird : MonoBehaviour { 
 
-    // Start is called before the first frame update
-    void Start()
+    public float upForce = 200f;
+
+    private bool isDead = false;
+    private Rigidbody2D rb2d;
+    private Animator anim;
+
+    // Use this for initialization
+    void Start ()
     {
-        rb2d = GetComponent<Rigidbody2D>();
+        rb2d = GetComponent<Rigidbody2D> ();
+        anim = GetComponent<Animator> ();
     }
 
     // Update is called once per frame
@@ -18,10 +22,18 @@ public class Bird : MonoBehaviour
     {
         if (isDead == false)
         {
-            if (Input.GetMousebuttonDown(0))
+            if (Input.GetMouseButtonDown(0))
             {
                 rb2d.velocity = Vector2.zero;
+                rb2d.AddForce (new Vector2 (0, upForce));
+                anim.SetTrigger ("Flap");
             }
         }
+    }
+
+    void OnCollisionEnter2D ()
+    {
+        isDead = true;
+        anim.SetTrigger ("Die");
     }
 }
