65437a125918bd03f645a4c31d85d2d8891b52a9
NoahS-132 <769161@student.dusd.net>
2025-12-16 13:13:05 -0800

    Finshed
    Flappy bird final finshed

--- SanchezFlappyFinalP4/Assets/Scripts/GameControl.cs
+++ SanchezFlappyFinalP4/Assets/Scripts/GameControl.cs
@@ -1,12 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
-public class GameControl : MonoBehaviour { 
+public class GameControl : MonoBehaviour {
 
     public static GameControl instance;
     public GameObject gameOverText;
+    public Text scoreText;
     public bool gameOver = false;
+    public float scrollSpeed = -1.5f;
+
+    private int score = 0;
 
     // Use this for initialization
     void Awake ()
@@ -20,13 +26,27 @@ public class GameControl : MonoBehaviour {
     }
 
     // Update is called once per frame
-    void Update()  { 
+    void Update()
+    {
+        if (gameOver == true && Input.GetMouseButtonDown (0))
+        {
+            SceneManager.LoadScene (SceneManager.GetActiveScene ().buildIndex);
+        }
+    }
 
-     }
-    
+    public void BirdScored()
+    {
+        if (gameOver)
+        {
+            return;
+        }
+        score++;
+        scoreText.text = "Score: " + score.ToString ();
+    }
+
     public void BirdDied()
     {
-        gameOverText.SetActive (true);
+        gameOverText.SetActive(true);
         gameOver = true;
     }
 }
